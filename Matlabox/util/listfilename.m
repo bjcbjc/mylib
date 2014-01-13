d38be1276690e0a1312fba729c5f3caa5846625f
bjchen <bjchen@nygenome.org>
2014-01-13 18:39:50 -0500

    timediff: specify directory (wildcard accepted); lifefilename: option to use ls instead of dir in matlab since ls supports wildcards in both directory and file names

--- Matlabox/util/listfilename.m
+++ Matlabox/util/listfilename.m
@@ -1,6 +1,13 @@
-function fns = listfilename(pattern)
-    f = dir(pattern);
-    fns = cell(size(f));
-    for i = 1:length(fns)
-        fns{i} = f(i).name;
+function fns = listfilename(pattern, usels)
+    if nargin < 2, usels = false; end
+    if ~usels
+        f = dir(pattern);
+        fns = cell(size(f));
+        for i = 1:length(fns)
+            fns{i} = f(i).name;
+        end
+    else
+        f = ls(pattern);
+        fns = textscan(f, '%s');
+        fns = fns{1};
     end
--- shlib/timediff.sh
+++ shlib/timediff.sh
@@ -3,8 +3,14 @@
 
 ext1=$1
 ext2=$2
+if [ $# -lt 3 ]; then
+    dir=""
+else
+    dir=$3
+fi
 
-for f in $(ls *.$ext1); do
+
+for f in $(ls $dir*.$ext1); do
     fn="${f%.*}"
     if [ -e $fn.$ext2 ]; then
 	hr=$(echo "scale=1; ($(date -r $fn.$ext2 +%s) - $(date -r $f +%s))/3600 " | bc)
