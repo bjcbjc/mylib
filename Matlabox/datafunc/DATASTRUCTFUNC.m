953bc1888657b25dc334034ca6cbb4ed77f63d0c
bjchen <bjchen@nygenome.org>
2014-02-14 17:19:18 -0500

    functions for datastrcut

--- Matlabox/datafunc/DATASTRUCTFUNC.m
+++ Matlabox/datafunc/DATASTRUCTFUNC.m
@@ -1,7 +1,10 @@
-classdef CROSSREF < handle
+classdef DATASTRUCTFUNC < handle
     
     methods (Static = true)
+        % query data.(keyfield) for "queries" and return all data in
+        % queryFields with matching queries
         function [result, idx] = queryData(data, keyfield, queries, queryFields)
+            
             if ischar(queryFields)
                 queryFields = {queryFields};
             end
@@ -115,7 +118,77 @@ classdef CROSSREF < handle
                 end
             end
         end
-                
+            
+        % order all fields in datastruct according to datastruct.(keyfield)
+        % and orderBy (new order)
+        function newdb = orderDataStruct(datastruct, keyfield, orderBy)
+            % input:
+            %   dbstruct, structure containing the database
+            %   fd2align: string, the fieldname to align
+            %   alignlist: cell array, list to align to
+            %
+            % Note: for those that are not in alignlist, create NaN in the matrix
+            %
+            [~, i] = ismember(orderBy, datastruct.(keyfield));
+            
+            if all(i==0)
+                error('nothing matched! Use your brain!');
+            end
+            
+            fds = fieldnames(datastruct);
+            nfd = length(fds);
+            dim2change = zeros(nfd,1);
+            ndim = zeros(nfd,1);
+            
+            matchdim = length(datastruct.(keyfield));
+            for j = 1:nfd
+                dim = size(datastruct.(fds{j}));
+                ndim(j) = length(dim);
+                for di = 1:ndim(j)
+                    if dim(di) == matchdim
+                        dim2change(j) = di;
+                        break
+                    end
+                end
+            end
+            
+            newlen = length(orderBy);
+            for j = 1:nfd
+                if dim2change(j) == 0
+                    newdb.(fds{j}) = datastruct.(fds{j});
+                else
+                    if strcmp(fds{j}, keyfield)
+                        newdb.(fds{j}) = orderBy;
+                    else
+                        ifsparse = issparse(datastruct.(fds{j}));
+                        permorder = [dim2change(j) setdiff(1:ndim(j), dim2change(j))];
+                        backorder = NaN(1, ndim(j));
+                        backorder(dim2change(j)) = 1;
+                        backorder(setdiff(1:ndim(j), dim2change(j))) = 2:ndim(j);
+                        d = permute(datastruct.(fds{j}), permorder);
+                        dim = size(datastruct.(fds{j}));
+                        newdim = dim(permorder);
+                        newdim(1) = newlen;
+                        if iscell(d)
+                            newdb.(fds{j}) = cell(newdim);
+                        else
+                            newdb.(fds{j}) = NaN(newdim);
+                        end
+                        s = '';
+                        for k = 1:ndim-1
+                            s = [s ',:'];
+                        end
+                        s = [s ')'];
+                        eval(sprintf('newdb.(fds{j})(i~=0%s = d(i(i~=0)%s;',s, s));
+                        newdb.(fds{j}) = permute(newdb.(fds{j}), backorder);
+                        if ifsparse
+                            newdb.(fds{j}) = sparse(newdb.(fds{j}));
+                        end
+                    end
+                end
+            end
+        end            
+        
         function newvar = allocateData(datatype, allocSize)
             if strcmp(datatype, 'cell')
                 newvar = cell(allocSize);
